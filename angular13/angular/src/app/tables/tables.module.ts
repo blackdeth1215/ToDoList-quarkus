e7e8ac218f51452c720aa5aebe0f60e29ebd2a8f
blackdeth1215 <68961164+blackdeth1215@users.noreply.github.com>
2022-08-02 05:57:54 -0500

    1.11
    1.11

---
+++ angular13/angular/src/app/tables/tables.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TablesComponent } from './tables.component';
+
+
+
+@NgModule({
+  declarations: [
+    TablesComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    TablesComponent
+  ]
+})
+export class TablesModule { }
