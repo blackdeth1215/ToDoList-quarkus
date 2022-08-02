ed6c78ab13f0435aae62c9f3c253813e4e9612f7
blackdeth1215 <68961164+blackdeth1215@users.noreply.github.com>
2022-08-01 20:33:52 -0500

    1.06
    1.06

--- angular13/loging/src/app/app-routing.module.ts
+++ angular13/loging/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, {
+    path: 'sing-in', loadChildren: () => import('./pages/aut/sing-in/sing-in.module').then(m => m.SingInModule) }, {
+      path: 'sing-up', loadChildren: () => import('./pages/aut/sing-up/sing-up.module').then(m => m.SingUpModule) },
+      {path:'**', redirectTo:'/home', pathMatch:'full'},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
